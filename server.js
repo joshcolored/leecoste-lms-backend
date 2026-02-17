6b22769c52d2a86f158f48b2fb8d29f3c2406559
Joshcolored <72875954+joshcolored@users.noreply.github.com>
2026-02-17 15:12:59 +0800

    Reformat server.js to remove unnecessary lines

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.get("/api/stats", async (req, res) => {
 
 /* ================= USER STATS (CHART) ================= */
 
-app.get("/api/user-stats", auth, async (req, res) => {
+app.get("/api/user-stats", async (req, res) => {
   try {
     const { range, from, to, type } = req.query;
 
@@ -356,3 +356,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
